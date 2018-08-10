988ca9de93fe6de107724c003a5b2a7ef0a8b49f
gyfastas <35861032+gyfastas@users.noreply.github.com>
2018-08-10 10:56:43 +0800

    Make it into int main()

--- music_player_2/music_player_2.ino
+++ music_player_2/music_player_2.ino
@@ -13,48 +13,116 @@ Rev.0 2018.8.9
 #include<MsTimer2.h>
 #include<String.h>
 //some note
-#define do0 261
-#define rui0 294
-#define mi0  330
-#define fa0 349
-#define so0 392
-#define la0 440
-#define xi0 494
-#define do1 523
-#define rui1 587
-#define mi1 659
-#define fa1 698
-#define so1 784
-#define la1 880
-#define xi1 988
-#define do2 1045
-#define rui2 1174
-#define mi2 1318
-#define fa2 1397
-#define so2 1568
-#define la2 1760
-#define xi2 1975
+#define xi0  31
+#define do1  33
+#define doS1 35
+#define rui1  37
+#define ruiS1 39
+#define mi1  41
+#define fa1  44
+#define faS1 46
+#define so1  49
+#define soS1 52
+#define la1  55
+#define laS1 58
+#define xi1  62
+#define do2  65
+#define doS2 69
+#define rui2  73
+#define ruiS2 78
+#define mi2  82
+#define fa2  87
+#define faS2 93
+#define so2  98
+#define soS2 104
+#define la2  110
+#define laS2 117
+#define xi2  123
+#define do3  131
+#define doS3 139
+#define rui3  147
+#define ruiS3 156
+#define mi3  165
+#define fa3  175
+#define faS3 185
+#define so3  196
+#define soS3 208
+#define la3  220
+#define laS3 233
+#define xi3  247
+#define do4  262
+#define doS4 277
+#define rui4  294
+#define ruiS4 311
+#define mi4  330
+#define fa4  349
+#define faS4 370
+#define so4  392
+#define soS4 415
+#define la4  440
+#define laS4 466
+#define xi4  494
+#define do5  523
+#define doS5 554
+#define rui5  587
+#define ruiS5 622
+#define mi5  659
+#define fa5  698
+#define faS5 740
+#define so5  784
+#define soS5 831
+#define la5  880
+#define laS5 932
+#define xi5  988
+#define do6  1047
+#define doS6 1109
+#define rui6  1175
+#define ruiS6 1245
+#define mi6  1319
+#define fa6  1397
+#define faS6 1480
+#define so6  1568
+#define soS6 1661
+#define la6  1760
+#define laS6 1865
+#define xi6  1976
+#define do7  2093
+#define doS7 2217
+#define rui7  2349
+#define ruiS7 2489
+#define mi7  2637
+#define fa7  2794
+#define faS7 2960
+#define so7  3136
+#define soS7 3322
+#define la7  3520
+#define laS7 3729
+#define xi7  3951
+#define do8  4186
+#define doS8 4435
+#define rui8  4699
+#define ruiS8 4978
+
 
 
 //beep on pin 9
-const int beep = 9;
+const int beep = 11;
 //button on pin 10
 const int button_1 = 10;
 //time period for interrupt (ms)
-const int timer2_period = 10;
+const int timer2_period = 12;
 
 //variable for music play			
 int audio_ptr=0;
 // use pace to modify your music duration							
-float pace=1.5;  		
+float pace=0.8;  		
 //play_flag
 int play_flag =0;
-
+//go flag
+int go_flag = 0;
 //this is the ISP
 void T_Handler();
 
-//music_player function
-void music_player();
 
 //Transmit event
 void Transmit_event();
@@ -65,27 +133,12 @@ unsigned int key_flag = 0;
 unsigned int key_timer = 0;
 const unsigned int period = 1;//here we do a period*20ms on/off 
 String p_s;
-int melody[200][2]=
-{
-      {la1,200},{do2,200},{rui2,200},{rui2,200},{rui2,200},{mi2,200},{mi2,400},{mi2,200},{mi2,200},
-              {so2,200},{la2,200},{rui2,200},{do2,200},{mi2,400},{la1,200},{do2,200},
-              {rui2,200},{rui2,200},{rui2,200},{mi2,200},{mi2,400},{mi2,200},{mi2,200},
-              {fa2,200},{mi2,200},{rui2,200},{do2,200},{do2,400},{la1,200},{do2,200},
-              {rui2,200},{rui2,200},{rui2,200},{mi2,200},{mi2,400},{mi2,200},{mi2,200},
-              {so2,200},{la2,200},{rui2,200},{do2,200},{mi2,400},{la1,200},{do2,200},
-                    {fa2,400},{mi2,400},{rui2,400},{do2,400},{rui2,200},{mi2,200},{xi1,200},{so1,200},{la1,400},{la1,200},{do2,200},
-                    {rui2,200},{rui2,200},{rui2,200},{mi2,200},{mi2,400},{mi2,200},{mi2,200},
-                                  {so2,200},{la2,200},{rui2,200},{do2,200},{mi2,400},{la1,200},{do2,200},
-                                  {rui2,200},{rui2,200},{rui2,200},{mi2,200},{mi2,400},{mi2,200},{mi2,200},
-                                  {fa2,200},{mi2,200},{rui2,200},{do2,200},{do2,400},{la1,200},{do2,200},
-                                  {rui2,200},{rui2,200},{rui2,200},{mi2,200},{mi2,400},{mi2,200},{mi2,200},
-                                  {so2,200},{la2,200},{rui2,200},{do2,200},{mi2,400},{la1,200},{do2,200},
-              {fa2,400},{mi2,400},{rui2,400},{do2,400},{rui1,200},{do1,200},{mi1,200},{so1,200},{la1,400},{0,0}
-};
+
 
 //FSM
 enum state{MENU,WRITE};
 state st,pre_st,next_st;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -99,34 +152,90 @@ void setup() {
 
   //__version__2 : FSM
   st = pre_st = next_st = MENU;
+
+  int melody[300][2]=
+{
+    {rui6,200},{mi6,200},{la5,100},{so5,100},{la5,100},{so5,100},{rui6,200},{mi6,200},{la5,100},{so5,100},{la5,100},{so5,100},{rui6,200},{mi6,200},{la5,100},{so5,100},{la5,100},{so5,100},{do1,200},{xi0,200},{la5,200},{so5,200},
+        {rui6,200},{mi6,200},{la5,100},{so5,100},{la5,100},{so5,100},{rui6,200},{mi6,200},{la5,100},{so5,100},{la5,100},{so5,100},
+        {rui6,200},{mi6,200},{so6,200},{do7,200},{xi6,100},{do7,100},{xi6,100},{la6,100},{so6,200},{mi6,200},
+        {rui6,200},{mi6,200},{la5,100},{so5,100},{la5,100},{so5,100},{rui6,200},{mi6,200},{la5,100},{so5,100},{la5,100},{so5,100},
+        {rui6,200},{mi6,200},{la5,100},{so5,100},{la5,100},{so5,100},{do1,200},{xi0,200},{la5,200},{so5,200},
+        {la5,200},{so5,100},{la5,100},{do1,200},{la5,100},{do1,100},{rui6,200},{rui6,100},{mi6,100},{so6,100},{do7,100},{mi6,100},{so6,100},
+        {do7,400},{xi6,133},{do7,133},{xi6,133},{la6,200},{so6,200},{la6,400},{la6,200},{do7,200},{0,200},
+        //����
+        {la6,250},{la6,150},{0,50},{so6,100},{la6,200},{do7,200},{rui7,200},{mi7,200},
+        {la6,250},{la6,150},{0,50},{so6,100},{la6,200},{so6,200},{mi6,200},{so6,200},
+        {la6,250},{la6,150},{la6,150},{0,50},{so6,100},{la6,200},{do7,200},{rui7,200},{mi7,200},
+        {mi7,400},{rui7,400},{do7,400},{la6,400},
+        {la6,200},{la6,200},{so6,100},{la6,200},{do7,200},{rui7,200},{mi7,200},
+        {la6,200},{la6,200},{so6,100},{la6,200},{so6,200},{so6,200},{mi6,200},
+        {la6,200},{la6,200},{so6,100},{so6,200},{la6,200},{do7,200},{rui7,200},
+        {mi7,400},{rui7,400},{do7,400},{la6,400},
+        //
+        {do7,400},{xi6,400},{la6,400},{so6,400},
+        {so6,200},{so6,100},{la6,100},{mi6,200},{rui6,200},{mi6,400},{0,200},
+        {mi6,200},{so6,200},{la6,400},{rui7,400},{xi6,400},{0,200},
+        {do7,400},{xi6,200},{so6,200},{la6,400},
+        {do7,400},{xi6,400},{la6,400},{so6,400},
+        {so6,200},{so6,100},{la6,100},{mi6,200},{rui6,200},{mi6,400},{mi6,200},{so6,200},
+        {la6,200},{la6,200},{la6,200},{la6,200},{do7,400},{rui7,400},{xi6,400},{0,200},
+        //�߳�
+        {la6,200},{do7,200},{rui7,200},{rui7,200},{rui7,200},{mi7,200},{mi7,400},{mi7,200},{mi7,200},
+        {so7,200},{la7,200},{rui7,200},{do7,200},{mi7,400},{la6,200},{do7,200},
+        {rui7,200},{rui7,200},{rui7,200},{mi7,200},{mi7,400},{mi7,200},{mi7,200},
+        {fa7,200},{mi7,200},{rui7,200},{do7,200},{do7,400},{la6,200},{do7,200},
+        {rui7,200},{rui7,200},{rui7,200},{mi7,200},{mi7,400},{mi7,200},{mi7,200},
+        {so7,200},{la7,200},{rui7,200},{do7,200},{mi7,400},{la6,200},{do7,200},
+              {fa7,400},{mi7,400},{rui7,400},{do7,400},{rui7,200},{mi7,200},{xi6,200},{so6,200},{la6,400},{la6,200},{do7,200},
+              {rui7,200},{rui7,200},{rui7,200},{mi7,200},{mi7,400},{mi7,200},{mi7,200},
+                            {so7,200},{la7,200},{rui7,200},{do7,200},{mi7,400},{la6,200},{do7,200},
+                            {rui7,200},{rui7,200},{rui7,200},{mi7,200},{mi7,400},{mi7,200},{mi7,200},
+                            {fa7,200},{mi7,200},{rui7,200},{do7,200},{do7,400},{la6,200},{do7,200},
+                            {rui7,200},{rui7,200},{rui7,200},{mi7,200},{mi7,400},{mi7,200},{mi7,200},
+                            {so7,200},{la7,200},{rui7,200},{do7,200},{mi7,400},{la6,200},{do7,200},
+        {fa7,400},{mi7,400},{rui7,400},{do7,400},{rui6,200},{do1,200},{mi6,200},{so6,200},{la6,400},{0,0}
+};
+
   
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+  while(1)
+  {
   //FSM State Change
   if(next_st !=st)
   { st = next_st;
     pre_st = st;
   }
+  if(play_flag)
+  { 
+    play_flag = 0;
+    if(melody[audio_ptr][0]==0&&melody[audio_ptr][1]==0)
+    {
+      audio_ptr=0;
+    }
+    else
+    {
+      tone(beep,melody[audio_ptr][0],melody[audio_ptr][1]*pace);
+      audio_ptr++;
+    }
+  }
   //
   if(key_flag)
   {
     key_flag = 0;
-    music_player();
+    play_flag = 1;
   }
   Transmit_event();
+  }
 }
+
 
 
 //20ms timer ISP
 void T_Handler()
 { 
-  //music_play
-  if(play_flag)
+  //auto play
+  if(go_flag)
   {
-    music_player();
+    play_flag = 1 - play_flag;
   }
   //frequency + duration =0 -> end of the music
   switch(key_state)
@@ -176,19 +285,6 @@ void T_Handler()
   } 
 }
 
-//music_player module used to play music
-void music_player()
-{
-    if(melody[audio_ptr][0]==0&&melody[audio_ptr][1]==0)
-  {
-    audio_ptr = 0;
-  }
-  else 
-  {
-    tone(beep,melody[audio_ptr][0],melody[audio_ptr][1]*pace);
-    audio_ptr++;
-  }
-}
 
 void Transmit_event()
 {
@@ -201,11 +297,12 @@ void Transmit_event()
       case MENU:
            if(p_s.equals("play"))
            {
-            play_flag = 1;
+            go_flag = 1;
             Serial.print("play");
            }
            if(p_s.equals("pause"))
            {
+            go_flag = 0;
             play_flag = 0;
             Serial.print("Pause");
            }
@@ -214,6 +311,7 @@ void Transmit_event()
             Serial.print("The music is stop now.");
             audio_ptr = 0;
             play_flag = 0;
+            go_flag = 0;
            }
            if(p_s.equals("Write"))
            {
